3a7dfe7801c98ff4913a2fc4b42752c652457281
Manal4888 <83536966+Manal4888@users.noreply.github.com>
2025-12-01 11:41:45 +0300

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 00:39:14 2018
+
+@author: louay
+"""
+
+n = int(input("Enter an integer n:"))
+if n<0:
+     print("Negative number!")
+else:
+     fact=1;
+     i=1
+     while i<=n:
+         fact = fact*i
+         i=i+1
+     print("Factorial of n:", fact)
+     
